5bc4336a77555526a3dffbe95fa58ea84cdf04d3
gloverac08 <glover.amy@gmail.com>
2018-03-15 18:52:38 -0500

    Implement board init functions

--- server/index.js
+++ server/index.js
@@ -31,3 +31,69 @@ app.get('*', (req, res) => res.redirect('/'));
 app.listen(process.env.PORT || 3000, function () {
   console.log('listening on port 3000!');
 });
+
+// Game State starters
+
+const coordinateGenerator = (numRows, numCols) => { // creates an array of coordinates for hexes
+  let j = 0;
+  let rowLength = numRows - 1;
+  let hexes = [];
+  
+  const rowGenerator = (rowIndex, firstCol) => { // creates a row in the grid
+    let iterations;
+    let row = []
+    if (rowIndex % 2 === 0) {
+      iterations = rowLength;
+    } else {
+      iterations = rowLength - 1;
+    }
+    
+    for (let i = 0; i < iterations; i++) {
+      row.push([firstCol, rowIndex]);
+      firstCol++;
+    }
+    return row;
+  }
+  
+  for (let i = 0; i < numRows; i++) {
+    hexes = hexes.concat(rowGenerator(i, j))
+    if (i % 2 !== 0) {
+      j--;
+    }
+  }
+  return hexes;
+}
+
+const isResourceHex = () => { // decides if hex gets resource
+  return Math.floor(Math.random() * (5 - 1) + 1) % 4 === 0;
+};
+
+const gameInit = () => { // creates an array of hexes with properties (the board)
+  let i = 0;
+  const hexes = coordinateGenerator(5);
+
+    return hexes.map((letter, index) => {
+      let hex = {};
+      hex.coordinates = letter;
+      
+      if (index === 0) {
+        hex.player = 'player1';
+        hex.units = 10;
+      } else if (index === hexes.length - 1) {
+        hex.player = 'player2';
+        hex.units = 10;
+      } else {
+        hex.player = null;
+        hex.units = 0;
+      }
+      if (isResourceHex() && index !== 0 && index !== 17) {
+        hex.hasResource = true;
+      } else {
+        hex.hasResource = false;
+      }
+      return hex;
+    });
+};
+
+
+
