1f9e66989fe57d9b71d648b7f96ce9731834944d
Stephanie Chou <steph015@gmail.com>
2018-03-17 14:35:26 -0500

    create db adduser and checkuser functions

--- database/index.js
+++ database/index.js
@@ -6,8 +6,59 @@ const knex = require('knex')({
   connection: config.mySql
 });
 
+// Adds users to db if does not exist
+const addUser = async (req, username, email, password) => {
+  const existingUser = await knex.select()
+    .from('users')
+    .where(knex.raw(`LOWER(username) = LOWER('${username}')`));
+
+  if (existingUser.length) {
+    return 'User already exists! :(';
+  } else {
+    return knex('users')
+      .insert({
+        username: username,
+        email: email,
+        password: password
+      });
+  }
+}
+
+// Checks user credentials
+const checkUserCreds = (username) => {
+  return knex.select()
+    .from('users')
+    .where(knex.raw(`LOWER(username) = LOWER('${username}')`));
+}
+
+// Create game
+// const createGame = async (game) => {
+//   return await knex('games')
+//     .insert({
+//       room_id: game.room_id,
+//       player1: game_player1,
+//       player2: game.player2,
+//       current_player: game.player1
+//     });
+//   // Create hexes
+//   await knex('hex')
+//     .insert({
+//       game_id:
+//       player:
+//       units: 
+//       has_resource:
+//     })
+// }
+// Delete game after certain amount of time has passed
+// If game has completed, update game to completed
+  // Update user wins
+  // Update user losses
+// Update player on hex if player moves to hex
+// Increase units
+// Decrease units
 
 
 module.exports = {
-
+  addUser,
+  checkUserCreds
 }
