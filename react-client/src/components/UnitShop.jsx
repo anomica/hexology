36bca6c831c1c1f6d13e4f5a60bf2bd6cca7af63
gloverac08 <glover.amy@gmail.com>
2018-03-22 20:03:09 -0500

    Implement UnitShop Card

--- react-client/src/components/UnitShop.jsx
+++ react-client/src/components/UnitShop.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Icon, Button, Transition, Header, Popup, Image, Modal, Content, Description, Label, Sidebar, Segment, Menu } from 'semantic-ui-react';
+import { Card, Icon, Button, Transition, Header, Popup, Image, Modal, Content, Description, Label, Sidebar, Segment, Menu } from 'semantic-ui-react';
 import { updateResources, swordsmen, archers, knights } from '../../src/actions/actions.js';
 
 class UnitShop extends React.Component {
@@ -119,52 +119,30 @@ class UnitShop extends React.Component {
       <div>
         {/* <Button onClick={this.toggleVisibility.bind(this)}>UnitShop</Button> */}
         {console.log('this.props.showUnitShop:', this.props.showUnitShop)}
-
-        <Menu vertical>
-          <Menu.Item name='bio'  />
-          <Menu.Item name='pics'  />
-          <Menu.Item name='companies'/>
-          <Menu.Item name='links' />
-        </Menu>
-        {/* <Popup trigger={<Button style={{marginTop: '30px'}} secondary onClick={() => this.show('blurring')}>Unit Store</Button>}>
-          <Popup.Header>Spend your resources on new units!</Popup.Header>
-        </Popup>
-
-        <Modal open={this.state.open} className={'unitShop'} size={'small'}
-          style={{textAlign: 'center'}} closeIcon onClose={this.close.bind(this)}>
-          <Modal.Header>Unit Shop</Modal.Header>
-          <Modal.Content>
-            <Modal.Description>
-              Your Resources: {this.props.userPlayer === 'player1' ?
-              `${this.props.playerOneResources.gold} Gold, ${this.props.playerOneResources.wood} Wood, ${this.props.playerOneResources.metal} Metal` :
-              `${this.props.playerTwoResources.gold} Gold, ${this.props.playerTwoResources.wood} Wood, ${this.props.playerTwoResources.metal} Metal`
-            }
-            </Modal.Description>
-            <Modal.Description>
-              <Transition  animation={'jiggle'} duration={'1000'} visible={this.state.swordsmen}>
-                <Label color='blue' image className={'unitType'} onClick={this.buySwordsmen.bind(this)}>
-                  <Image src="https://png.icons8.com/metro/50/000000/sword.png"/>
-                  Swordsmen
-                  <Label.Detail>Cost: 10 gold, 10 metal</Label.Detail>
-                </Label>
-              </Transition>
-              <Transition animation={'jiggle'} duration={'1000'} visible={this.state.archers}>
-                <Label color='green' image className={'unitType'} onClick={this.buyArchers.bind(this)}>
-                  <Image src="https://png.icons8.com/windows/50/000000/archer.png"/>
-                  Archer
-                  <Label.Detail>Cost: 10 gold, 20 wood</Label.Detail>
-                </Label>
-              </Transition>
-              <Transition animation={'jiggle'} duration={'1000'} visible={this.state.knights}>
-                <Label color='grey' image className={'unitType'} onClick={this.buyKnights.bind(this)}>
-                  <Image src="https://png.icons8.com/ios/50/000000/knight-shield-filled.png"/>
-                  Knight
-                  <Label.Detail>Cost: 20 gold, 20 metal, 20 wood</Label.Detail>
-                </Label>
-              </Transition>
-            </Modal.Description>
-          </Modal.Content>
-        </Modal> */}
+        <Card>
+          <Card.Header>Purchase Units</Card.Header>
+          <Card.Content>
+            <Label color='blue' image className={'unitType'} onClick={this.buySwordsmen.bind(this)}>
+            <Image src="https://png.icons8.com/metro/50/000000/sword.png" />
+              Swordsmen
+            </Label>
+            <Card.Description>Cost: 10 gold, 10 metal</Card.Description>
+          </Card.Content>
+          <Card.Content>
+            <Label color='green' image className={'unitType'} onClick={this.buyArchers.bind(this)}>
+              <Image src="https://png.icons8.com/windows/50/000000/archer.png" />
+              Archer
+            </Label>
+            <Card.Description>Cost: 10 gold, 20 wood</Card.Description>
+          </Card.Content>
+          <Card.Content>
+            <Label color='grey' image className={'unitType'} onClick={this.buyKnights.bind(this)}>
+              <Image src="https://png.icons8.com/ios/50/000000/knight-shield-filled.png" />
+               Knight
+            </Label>
+          <Card.Description>Cost: 20 gold, 20 metal, 20 wood</Card.Description>
+          </Card.Content>
+        </Card>
       </div>
     )
   }
